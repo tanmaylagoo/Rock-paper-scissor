218d75e610c822db5c1c069b90e13f1a3c5d72e0
tanmaylagoo <tanmaylagoo@gmail.com>
2025-04-03 22:21:42 +0530

    Update rps.py
    updated rps(rock, paper, scissor)

--- rps.py
+++ rps.py
@@ -4,41 +4,21 @@ import random
 0 is for paper
 -1 is for scissors
 '''
-computer=random.choice([-1,0,1])
-you=input("Enter your choice:").lower()
+computer = random.choice([-1, 0, 1])
+you = input("Enter your choice (rock, paper, scissor): ").lower()
 
-game_dict={"rock":1,
-           "paper":0,
-           "scissor":-1
-           }
-rev_dict={1:"rock",
-           0:"paper",
-           -1:"scissor"
-           }
-num=game_dict[you]
-print(f"You chose {rev_dict[num]} \nComputer chose {rev_dict[computer]}\nTherefore,")
-if (computer-num==1 or computer-num==-2):
-    print("You win!")
-if (computer==num):
-    print("Tie!")
-else:
-    print("You lose!")
+game_dict = {"rock": 1, "paper": 0, "scissor": -1}
+rev_dict = {1: "rock", 0: "paper", -1: "scissor"}
 
-'''if computer==num:
-    print("Tie")
+if you not in game_dict:
+    print("Invalid choice! Please enter rock, paper, or scissor.")
 else:
-    if (computer==0 and num==1): 
-        print("You Lose!")
-    elif (computer==0 and num==-1):
-        print("You Win!")
-    elif (computer==1 and num==0):
-        print("You Win!")
-    elif (computer==1 and num==-1):
-        print("You Lose!")
-    elif (computer==-1 and num==0):
-        print("You Lose!")
-    elif (computer==-1 and num==1):
-        print("You Win!")
+    num = game_dict[you]
+    print(f"You chose {rev_dict[num]} \nComputer chose {rev_dict[computer]}\nTherefore,")
+
+    if num == computer:
+        print("Tie!")
+    elif (computer - num == 1) or (computer - num == -2):
+        print("You win!")
     else:
-        print("Something went wrong!")'''
-
+        print("You lose!")
