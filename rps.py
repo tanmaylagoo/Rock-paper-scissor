61425c7da84a36c03226188e573d0622f6ffa1cf
tanmaylagoo <tanmaylagoo@gmail.com>
2025-04-06 12:55:36 +0530

    Update rps.py

--- rps.py
+++ rps.py
@@ -1,24 +1,26 @@
 import random
 
-'''1 is for rock
-0 is for paper
--1 is for scissors
 '''
-computer = random.choice([-1, 0, 1])
-you = input("Enter your choice (rock, paper, scissor): ").lower()
+1 → rock
+0 → paper
+-1 → scissor
+'''
 
-game_dict = {"rock": 1, "paper": 0, "scissor": -1}
 rev_dict = {1: "rock", 0: "paper", -1: "scissor"}
 
-if you not in game_dict:
-    print("Invalid choice! Please enter rock, paper, or scissor.")
-else:
-    num = game_dict[you]
-    print(f"You chose {rev_dict[num]} \nComputer chose {rev_dict[computer]}\nTherefore,")
-
-    if num == computer:
-        print("Tie!")
-    elif (computer - num == 1) or (computer - num == -2):
-        print("You win!")
+try:
+    you = int(input("Enter your choice (1 for rock, 0 for paper, -1 for scissor): "))
+    if you not in [-1, 0, 1]:
+        print("Invalid choice! Please enter 1, 0, or -1.")
     else:
-        print("You lose!")
+        computer = random.choice([-1, 0, 1])
+        print(f"You chose {rev_dict[you]} \nComputer chose {rev_dict[computer]}\nTherefore,")
+
+        if you == computer:
+            print("Tie!")
+        elif (computer - you == 1) or (computer - you == -2):
+            print("You win!")
+        else:
+            print("You lose!")
+except ValueError:
+    print("Invalid input! Please enter a number (1, 0, or -1).")
